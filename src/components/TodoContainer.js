91ae5e538985848282117146a86da673b71956ed
Paul Rainsford <paulrainsford69@gmail.com>
2022-03-30 22:02:39 +0100

    Added components to src folder

---
+++ src/components/TodoContainer.js
@@ -0,0 +1,12 @@
+import React from "react"
+class TodoContainer extends React.Component {
+    render() {
+        return (
+        <div>
+            <h1>Hello from Create React</h1>
+            <p>I am in a React Component!</p>
+        </div>
+        )
+    }
+}
+export default TodoContainer
