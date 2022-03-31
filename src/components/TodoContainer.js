9a80d7ae39aa152f0fb93483be1a53efd7466f4f
Paul Rainsford <paulrainsford69@gmail.com>
2022-03-31 22:09:34 +0100

    Added data to TodoContainer

--- src/components/TodoContainer.js
+++ src/components/TodoContainer.js
@@ -1,12 +1,38 @@
+import { findAllByDisplayValue } from "@testing-library/react"
 import React from "react"
 class TodoContainer extends React.Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title:
+                "Setup development environment",
+                completed: true
+            },
+            {
+                id: 2,
+                title: "Develop website and add content",
+                completed: false
+            },
+            {
+                id: 3,
+                title: "Deploy to live server",
+                completed: false
+            }
+        ]
+    };
     render() {
         return (
+            <ul>
+                {this.state.todos.map(todo => (
+                    <li>{todo.title}</li>
+                ))}
+            </ul>
+        );
         <React.Fragment>
             <h1>Hello from Create React</h1>
             <p>I am in a React Component!</p>
             </React.Fragment>
-        )
     }
 }
 export default TodoContainer
